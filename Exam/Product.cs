ef542126ea179c832ef5875ec5fa0c7c79bfe62c
akshaykarale <97279967+akshaykarale@users.noreply.github.com>
2022-02-19 18:56:56 +0530

    Add files via upload

---
+++ Exam/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationExam.Models
+{
+    public class Product
+    {   [Key]
+        public int ProductId { set; get; }
+        public String ProductName { set; get; }
+        public decimal Rate { set; get; }
+
+        public string Description { set; get; }
+
+        public string CategoryName { set; get; }
+
+    }
+}
